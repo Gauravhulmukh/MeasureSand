fde6bf6d3b6238614cf3dde0c95f74e69500bee0
Gaurav Hulmukh <37579974+Gauravhulmukh@users.noreply.github.com>
2020-09-20 21:42:10 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,26 @@
 var cacheName = 'measuresand';
-var cacheFiles = ['./','./index.html','./css/style.css','./js/main.js'];
+var filesToCache = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/main.js'
+];
 
-/* Start Service Worker and cache all content */
-self.addEventListener('install',function(e) {
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
   e.waitUntil(
-      caches.open(cacheName).then(function(cache){
-        return cache.addAll(cacheFiles);
-        });
-      );
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch',function(e){
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-  caches.match(e.request).then(function(response){
-    return response || fetch(e.request);
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
-   );
- });
+  );
+});
      
